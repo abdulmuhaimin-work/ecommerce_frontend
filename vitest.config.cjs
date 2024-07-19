d69bfa893f5099f5e42cac1fd7233d9c75fbb51d
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2024-07-19 21:41:00 +0800

    resolve esm/cjs syntax conflict

---
+++ vitest.config.cjs
@@ -0,0 +1,11 @@
+const { defineConfig } = require('vitest/config');
+const vue = require('@vitejs/plugin-vue');
+
+module.exports = defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.js'
+  }
+});
